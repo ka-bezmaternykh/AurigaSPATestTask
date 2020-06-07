3890bd3fe1cc2660945bc401ba55656b0f7e33b3
Kirill Bezmaternykh <ka.bezmaternykh@gmail.com>
2020-06-07 18:10:15 +0300

    Add initial dotnet's solution structure Add default example as first test case

---
+++ src/Auriga.Pebbles/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace Auriga.Pebbles
+{
+    public class Solution
+    {
+        public int MaxScore(int[] squares)
+        {
+            return 0;
+        }
+    }
+}
---
+++ tests/Auriga.Pebbles.Tests/SolutionTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Auriga.Pebbles.Tests
+{
+    [TestClass]
+    public class SolutionTests
+    {
+        [TestMethod]
+        public void InitialExample_ReturnsExpectedScore()
+        {
+            // Arrange
+            var squares = new int[] { 1, -2, 0, 9, -1, -2 };
+
+            // Act
+            var result = new Solution().MaxScore(squares);
+
+            // Assert
+            Assert.AreEqual(8, result, "Should return max sum of 8");
+        }
+    }
+}
